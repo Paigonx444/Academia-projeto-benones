d48319df41d16a3cb000ec42f30505b0f82ad7ca
pedro braga <pedro.bramos@eaportal.org>
2026-08-13 17:45:20 -0300

    Cadastro funcionando

--- buscarVendaCombustivel.js
+++ buscarVendaCombustivel.js
@@ -1,16 +1,16 @@
 async function listarTodos() {
-    const buscaNoBancoDeDados = await fetch('http://localhost:3000/vendaCombustivel');
+    const buscaNoBancoDeDados = await fetch('http://localhost:3000/cadastrarAluno');
     const respostaObtida = await buscaNoBancoDeDados.json();
     console.log(respostaObtida);
-    let html = '<table border="1"><tr><th>id</th><th>Tipo de Combustível</th><th>Preço</th><th>Volume Abastecido</th><th>Data Abastecimento</th></tr>';
+    let html = '<table border="1"><tr><th>id</th><th>Nome do aluno</th><th>Preço</th><th>CPF do aluno</th><th>Data do cadastro</th></tr>';
 
-    respostaObtida.forEach(vendaCombustivel => {
+    respostaObtida.forEach(cadastrarAluno => {
         html += `<tr>
-        <td>${vendaCombustivel.id}</td>
-        <td>${vendaCombustivel.tipo_combustivel}</td>
-        <td>${vendaCombustivel.preco}</td>
-        <td>${vendaCombustivel.volume_abastecido}</td>
-        <td>${vendaCombustivel.data_abastecimento}</td>
+        <td>${cadastrarAluno.id}</td>
+        <td>${vendaCombustivel.nome_aluno}</td>
+        <td>${vendaCombustivel.idade_aluno}</td>
+        <td>${vendaCombustivel.cpf_aluno}</td>
+        <td>${vendaCombustivel.data_cadastro}</td>
         </tr>`;
     });
 
